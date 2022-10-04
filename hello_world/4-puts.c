084841a42b684aaae301be37158ff5b1e5711349
John Naduer <naduer789@gmail.com>
2022-10-03 19:21:24 -0700

    my first program

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+*main is the primary function
+*
+*return 0 all good
+ */
 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
