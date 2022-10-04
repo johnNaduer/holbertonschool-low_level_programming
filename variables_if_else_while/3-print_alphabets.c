3a3846cf624a31e4b6f68d06f712d9a851460e60
John Naduer <naduer789@gmail.com>
2022-10-04 16:14:30 -0700

    by adding the capital letters of the alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n, m;
+
+	n = 'a';
+	m = 'A';
+
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+
+	while (m <= 'Z')
+	{
+		putchar(m + '$');
+		m++;
+	}
+	putchar('\n');
+
+	return (0);
+}
