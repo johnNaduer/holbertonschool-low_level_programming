1f0d328f34feae6c4176a50f15d185181288caf0
John Naduer <naduer789@gmail.com>
2022-10-04 15:41:20 -0700

    creating a program that prints the alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+
+	n = 'a';
+
+	while (n <= 'z')
+	{
+	putchar(n);
+	n++;
+	}
+	return (0);
+}
+
