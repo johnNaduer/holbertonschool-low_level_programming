35276d239cd4fdb7ca65e6d3b42f08aaf0763b69
John Naduer <naduer789@gmail.com>
2022-10-04 20:49:12 -0700

    combining the numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int A;
+
+	A = '0';
+
+	while (A <= '9')
+	{
+		putchar(A);
+	if (A >= '0' && A <= '8')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+		A++;
+	}
+	return (0);
+}
