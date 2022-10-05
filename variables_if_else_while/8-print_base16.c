1024b8e965ae79329c6da7f1e8979dc56acdc3aa
John Naduer <naduer789@gmail.com>
2022-10-04 20:02:17 -0700

    displaying base 16 numbers

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char y;
+	int x;
+
+	for (y = '0'; y <= '9'; y++)
+	{
+		putchar(y);
+	}
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
+
