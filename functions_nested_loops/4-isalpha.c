d34448051f32b4cf438046938efa73c655c115cd
John Naduer <naduer789@gmail.com>
2022-10-05 17:15:28 -0700

    Check the alphabet character

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  * _islower - Check the alphabet character
+  * _isalpha - Check the alphabet character
   * @c: The character to be checked
   * Return: 1 for lowercase character or 0 for anything else
   */
