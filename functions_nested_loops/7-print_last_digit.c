fbd255bd5fc692d81a5bdf0669b53849b32aa464
John Naduer <naduer789@gmail.com>
2022-10-06 22:44:51 -0700

    the last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,22 @@
 #include "main.h"
-
-
+/**
+ * print_last_digit - Prints the last digit
+ *
+ * @x: input number as an integer.
+ *
+ * Return: last digit.
+ */
 int print_last_digit(int x)
 {
 	int residuo;
-	
-	residuo=(x%10);
 
-	if(x < 0)
+	residuo = (x % 10);
+
+	if (x < 0)
 	{
-	residuo=residuo*(-1);
+	residuo = residuo * (-1);
 	}
 
-	_putchar(residuo+'0');
+	_putchar(residuo + '0');
 	return (residuo);
 }
