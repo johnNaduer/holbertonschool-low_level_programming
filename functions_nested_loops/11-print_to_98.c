9582187bdbd8b30cfe7dab4248c49ba80381560b
John Naduer <naduer789@gmail.com>
2022-10-07 08:36:16 -0700

    prints all numbers up to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - Prints all natural numbers
+ * from n to 98.
+ *
+ * @n: input number.
+ *
+ * Return: no return.
+ */
+	void print_to_98(int n)
+	{
 
+		int conta = 0;
+
+
+	while (conta == 0)
+	{
+		printf("%d", n);
+	if (n == 98)
+	{
+		conta = 1;
+	}
+	else
+	{
+		printf(", ");
+	}
+	if (n > 98)
+	{
+		n--;
+	}
+	else
+	{
+		n++;
+	}
+	}
+		printf("\n");
+	}
