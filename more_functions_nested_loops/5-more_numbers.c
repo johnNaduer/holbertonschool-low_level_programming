cced506fe2b017264ade64c3b1110509ceab28bb
John Naduer <naduer789@gmail.com>
2022-10-08 20:40:53 -0700

    function to draw a straight line on the terminal

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: no return
+ */
+void more_numbers(void)
+{
+        int i,j;
+	for(j= 0; j < 10; j++)
+	{
+        for (i = 0 ; i <= 14; i++)
+        {
+           
+	_putchar(i + '0');
+
+        }
+        _putchar('\n');
+	
+	}
+
+}
+
---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - prints lines
+ *@n :input integer variable
+ * Return: no return
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar('-');
+
+	}
+	_putchar('\n');
+}
