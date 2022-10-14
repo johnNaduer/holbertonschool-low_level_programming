34697d410298f0cd3aff78a2c3425fdd3e11b89e
John Naduer <naduer789@gmail.com>
2022-10-13 20:56:22 -0700

    function that copies a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int x;
 
-	for (x = 0; src[x] != '\0'; x++)
-		if (i < n)
-			dest[x] = src[x];
-		while (x < n)
-			dest[x++] = '\0';
-
+	x = 0;
+	while (x < n && *(src + x))
+	{
+		*(dest + x) = *(src + x);
+		x++;
+	}
+	while (x < n)
+	{
+		*(dest + x) = '\0';
+		x++;
+	}
 	return (dest);
 }
