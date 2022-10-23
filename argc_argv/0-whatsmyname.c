4d64c0164522aff63819afa9703de941d3ef5da7
John Naduer <naduer789@gmail.com>
2022-10-23 08:16:03 -0700

    program that prints its name, followed by a new line

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -6,13 +6,13 @@
  * @argv: input char
  * Return: Always 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-        int i;
+	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n",argv[i]);
+		printf("%s\n", argv[i]);
 	}
 
 	return (0);
