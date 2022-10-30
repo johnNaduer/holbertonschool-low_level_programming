878fce54e4529c893beb5a8b992070824db29388
John Naduer <naduer789@gmail.com>
2022-10-29 21:23:28 -0700

    function that concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -31,12 +31,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	for (j = 0; j < i; j++)
+	{
 		*(ptr + j) = *(s1 + j);
-
+	}
 	for (j = 0; j < z; j++)
+	{
 		*(ptr + i + j) = *(s2 + j);
-
-		ptr[i + z] = '\0';
-		return (ptr);
+	}
+	ptr[i + z] = '\0';
+	return (ptr);
 }
 
