259ae5da199e0436ef4053fd0df959d821d350a2
John Naduer <naduer789@gmail.com>
2022-12-03 16:45:30 -0800

    shell7.c

--- archivos_temporales/practice-shell/shell/shell7.c
+++ archivos_temporales/practice-shell/shell/shell7.c
@@ -17,11 +17,35 @@ char *cargar_texto(char *buf,size_t len)
 
 }
 
+char **toukenizar_lectura(char *lectura, char separador[])
+{
+
+	
+	char **guardar_argumentos;
+	char *tem;
+	char *token;
+	int i=0;
+	guardar_argumentos = calloc(10,sizeof(char *));
+	token = strtok(lectura,separador);
+	
+	while(token!=NULL)
+	{
+	printf("%s\n",token);
+	tem=strdup(token);
+	guardar_argumentos[i]=tem;
+	token = strtok(NULL,separador);
+	i++;
+	}
+}
+
+
 int main(void)
 {
         char *lectura;
         char *buffer = NULL;
         size_t len = 0;
+	char **toukenizado;
+	char separador[]=" ,\t\n";
 
 while (1)
 {
@@ -30,6 +54,9 @@ while (1)
 
         lectura = cargar_texto(buffer,len);
 	
+	toukenizado = toukenizar_lectura(lectura,separador);
+
+			
 	
 
 }
