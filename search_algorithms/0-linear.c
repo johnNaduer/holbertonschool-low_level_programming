fa1354e99795b248e479756dc074df0abfc4a7b9
johnNaduer <naduer789@gmail.com>
2023-01-04 17:11:24 -0500

    update: add correction standart betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -2,29 +2,40 @@
 #include <stdlib.h>
 #include "search_algos.h"
 
+/**
+ * linear_search - function that searches for a value in an array of integers
+ *
+ * @array: input int
+ * @size: input size_t
+ * @value: input int
+ *
+ * Return: i or -1
+ *
+ */
+
 int linear_search(int *array, size_t size, int value)
 {
 
-size_t i;
+	size_t i;
 
-if(array == NULL)
+if (array == NULL)
 {
-return (-1);
+	return (-1);
 }
-for(i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
-printf("value checked array[%ld] = [%d]\n",i,array[i]);
-if(array[i]==value)
+	printf("value checked array[%ld] = [%d]\n", i, array[i]);
+if (array[i] == value)
 {
-break;
+	break;
 }
 }
-if(array[i]==value)
+	if (array[i] == value)
 {
-return (i);
+	return (i);
 }
 else
 {
-return (-1);
+	return (-1);
 }
 }
